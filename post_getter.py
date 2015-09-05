3316847fda85eda5c5a7f1a72305dc0c729a036e
nice-shot <shefferu@gmail.com>
2015-09-05 19:14:47 +0300

    the post getter pretty much works. Now to start using it

--- post_getter.py
+++ post_getter.py
@@ -13,7 +13,8 @@ class PostGetter(object):
     POST_LIMIT = 30
     FIELDS = ["message", "created_time", "updated_time", "from"]
 
-    def __init__(self, group_id, keywords, token=None, last_id=None):
+    def __init__(self, group_id, keywords, token="", seen_posts=None,
+                 since="2015-09-01"):
         """
         :param group_id: The Facebook object ID for the group
         :type group_id: int
@@ -21,26 +22,56 @@ class PostGetter(object):
         :type keywords: list[str]
         :param token: User access token
         :type token: str
-        :param last_id: Last post that we checked
-        :type last_id: int
+        :param seen_posts: A list of post-ids that were already seen
+        :type seen_posts: list[str]
+        :param since: A date string used to filter old posts
+        :type since: str
         """
         self.log = logging.getLogger(__name__ + "." + str(group_id))
-        base_url = "https://graph.facebook.com/v2.0/{}/feed?{}".format(
+        self.keywords = keywords
+        self.seen_posts = seen_posts or []
+
+        self.base_url = "https://graph.facebook.com/v2.0/{}/feed?{}".format(
             group_id,
             urlencode({
                 "access_token": token,
+                "since": since,
                 "fields": ",".join(self.FIELDS),
                 "limit": self.POST_LIMIT
             })
         )
-        self.page_data = json.loads(urlopen(base_url).read().decode("utf-8"))
 
-        self.keywords = keywords
-        self.last_id = last_id
+    def get_post(self, url=None):
+        """
+        Starts getting posts from the given page. If the url is omitted - grabs
+        from the main page's feed
 
-    def get_post(self):
-        for post in self.page_data["data"]:
+        :param url: The page to grab feeds from
+        :type url: str
+        """
+        if not url:
+            url = self.base_url
+
+        self.log.info("Getting URL: %s", url)
+        page_data = json.loads(urlopen(url).read().decode("utf-8"))
+
+        for post in page_data.get("data", []):
+            if post["id"] == self.seen_posts:
+                continue
+
+            if "message" not in post:
+                continue
+
             for word in self.keywords:
+                if word in post["message"]:
+                    self.log.debug("Emitting post: %s", post["id"])
+                    yield post
+                    break
+
+        paging = page_data.get("paging", {})
+
+        if "next" in paging:
+            for post in self.get_post(paging["next"]):
                 yield post
 
         return
